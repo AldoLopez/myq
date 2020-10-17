cc330ab8fc791677677e55a3f3ea805f68b4c506
Aldo Lopez <lopez.aldo.a@gmail.com>
2020-10-17 00:36:32 -0400

    only in app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 import netlifyIdentity from 'netlify-identity-widget';
 import { loginUser, logoutUser } from './identityActions';
 import auth from './hoc/AuthHOC';
-import Garage from './Garage';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
-
+import { toggleLight } from './Toggles';
 netlifyIdentity.init();
 
 class App extends React.Component {
@@ -44,11 +43,12 @@ class App extends React.Component {
           <div>
             {this.state.user ? (
               <header className='App-header'>
-                <Link to='/garage' className='Garage' alt='garage'>
-                  <Button variant='contained' color='primary'>
-                    <span>Garage</span>
-                  </Button>
-                </Link>
+                <Button
+                  id='toggleLight'
+                  label='Toggle Light'
+                  color='primary'
+                  onClick={toggleLight}
+                ></Button>
                 <Button variant='contained' onClick={this.handleLogOut}>
                   Logout
                 </Button>
@@ -64,9 +64,6 @@ class App extends React.Component {
                 </Button>
               </header>
             )}
-            <section>
-              <Route exact path='/garage' component={auth(Garage)} />
-            </section>
           </div>
         </Router>
       </div>
--- src/Garage.js
+++ src/Garage.js
@@ -4,21 +4,6 @@ import axios from 'axios';
 import { generateHeaders } from './identityActions';
 import './Garage.css';
 
-const toggleLight = () => {
-  generateHeaders().then((headers) => {
-    axios
-      .get(
-        'https://gallant-jackson-3a417f.netlify.app/.netlify/functions/switchGarageLightState',
-        {
-          headers,
-        }
-      )
-      .then((res) => {
-        console.log(res);
-      });
-  });
-};
-
 const Garage = () => (
   <div>
     <Button
